babf7ae3c6ae3567ab701daf117c47876d5cd7a0
EMMANUEL TOWOJU <40261626+towoju5@users.noreply.github.com>
2022-04-10 22:58:26 +0100

    Update 2022_03_26_144125_create_shipping_methods_table.php

--- resources/database/migrations/2022_03_26_144125_create_shipping_methods_table.php
+++ resources/database/migrations/2022_03_26_144125_create_shipping_methods_table.php
@@ -11,7 +11,7 @@ return new class () extends Migration {
         Schema::create('shipping_methods', function (Blueprint $table) {
             $table->id();
             $table->string('name')->nullable();
-            $table->bigInteger('carrier_id')->nullable();
+            $table->unsignedBigInteger('carrier_id')->nullable();
             $table->json('configuration')->nullable();
             $table->timestamps();
 
